25efcd568eda0b54443ee3c7d8874ede6b14c937
Tobias Kuhn <kuhntobias@gmail.com>
2019-11-15 16:13:41 +0100

    Create the new admin structure for nanopubs (still needs testing)

--- src/main/java/nl/unimaas/ids/NanopubModule.java
+++ src/main/java/nl/unimaas/ids/NanopubModule.java
@@ -1,16 +1,18 @@
 package nl.unimaas.ids;
 
-
 import java.security.GeneralSecurityException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.TimeZone;
 
 import org.eclipse.rdf4j.RDF4JException;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.model.vocabulary.DCTERMS;
@@ -24,6 +26,8 @@ import org.nanopub.SimpleTimestampPattern;
 import org.nanopub.extra.security.MalformedCryptoElementException;
 import org.nanopub.extra.security.NanopubSignatureElement;
 import org.nanopub.extra.security.SignatureUtils;
+
+import net.trustyuri.TrustyUriUtils;
 
 public class NanopubModule {
 
@@ -40,14 +44,58 @@ public class NanopubModule {
 				containsNullCharacter = true;
 				npToLoad = new NanopubImpl(payload.replaceAll("\0", ""), format);
 			}
-			// TODO: check that nanopub doesn't use admin namespace
-			List<Statement> st = new ArrayList<>();
-			st.addAll(NanopubUtils.getStatements(npToLoad));
-			st.add(vf.createStatement(np.getUri(), HAS_HEAD_GRAPH, np.getHeadUri(), ADMIN_GRAPH));
+			List<Statement> statements = new ArrayList<>();
+			String ac = TrustyUriUtils.getArtifactCode(np.getUri().toString());
+			if (!np.getHeadUri().toString().contains(ac) || !np.getAssertionUri().toString().contains(ac) ||
+					!np.getProvenanceUri().toString().contains(ac) || !np.getPubinfoUri().toString().contains(ac)) {
+				conn.add(vf.createStatement(np.getUri(), NOTE, vf.createLiteral("could not load nanopub as not all graphs contained the artifact code"), ADMIN_GRAPH));
+				return;
+			}
+			Set<IRI> subIris = new HashSet<>();
+			Set<IRI> otherNps = new HashSet<>();
+			for (Statement st : NanopubUtils.getStatements(npToLoad)) {
+				statements.add(st);
+				if (st.getSubject().equals(np.getUri()) && st.getObject() instanceof IRI) {
+					if (st.getObject().toString().matches(".*[^A-Za-z0-9\\-_]RA[A-Za-z0-9\\-_]{43}")) {
+						statements.add(vf.createStatement(np.getUri(), st.getPredicate(), st.getObject(), ADMIN_NETWORK_GRAPH));
+					}
+				}
+				if (st.getSubject().toString().contains(ac)) {
+					subIris.add((IRI) st.getSubject());
+				} else {
+					IRI b = getBaseTrustyUri(st.getSubject());
+					if (b != null) otherNps.add(b);
+				}
+				if (st.getPredicate().toString().contains(ac)) {
+					subIris.add(st.getPredicate());
+				} else {
+					IRI b = getBaseTrustyUri(st.getPredicate());
+					if (b != null) otherNps.add(b);
+				}
+				if (st.getObject() instanceof IRI) {
+					if (st.getObject().toString().contains(ac)) {
+						subIris.add((IRI) st.getObject());
+					} else {
+						IRI b = getBaseTrustyUri(st.getObject());
+						if (b != null) otherNps.add(b);
+					}
+				}
+			}
+			subIris.remove(np.getUri());
+			subIris.remove(np.getAssertionUri());
+			subIris.remove(np.getProvenanceUri());
+			subIris.remove(np.getPubinfoUri());
+			for (IRI i : subIris) {
+				statements.add(vf.createStatement(np.getUri(), HAS_SUB_IRI, i, ADMIN_GRAPH));
+			}
+			for (IRI i : otherNps) {
+				statements.add(vf.createStatement(np.getUri(), REFERS_TO_NANOPUB, i, ADMIN_GRAPH));
+			}
+			statements.add(vf.createStatement(np.getUri(), HAS_HEAD_GRAPH, np.getHeadUri(), ADMIN_GRAPH));
 			try {
 				NanopubSignatureElement el = SignatureUtils.getSignatureElement(np);
 				if (el != null && SignatureUtils.hasValidSignature(el) && el.getPublicKeyString() != null) {
-					st.add(vf.createStatement(np.getUri(), HAS_VALID_SIGNATURE_FOR_PUBLIC_KEY, vf.createLiteral(el.getPublicKeyString()), ADMIN_GRAPH));
+					statements.add(vf.createStatement(np.getUri(), HAS_VALID_SIGNATURE_FOR_PUBLIC_KEY, vf.createLiteral(el.getPublicKeyString()), ADMIN_GRAPH));
 				}
 			} catch (GeneralSecurityException | MalformedCryptoElementException ex) {}
 			Calendar timestamp = null;
@@ -57,20 +105,20 @@ public class NanopubModule {
 				System.err.println("Illegal date/time for nanopublication " + np.getUri());
 			}
 			if (timestamp != null) {
-				st.add(vf.createStatement(np.getUri(), CREATION_DAY, vf.createIRI(NPA_DATE_PREFIX + getDayString(timestamp)), ADMIN_GRAPH));
-				st.add(vf.createStatement(np.getUri(), CREATION_MONTH, vf.createIRI(NPA_DATE_PREFIX + getMonthString(timestamp)), ADMIN_GRAPH));
-				st.add(vf.createStatement(np.getUri(), CREATION_YEAR, vf.createIRI(NPA_DATE_PREFIX + getYearString(timestamp)), ADMIN_GRAPH));
-				st.add(vf.createStatement(np.getUri(), DCTERMS.CREATED, vf.createLiteral(timestamp.getTime()), ADMIN_GRAPH));
+				statements.add(vf.createStatement(np.getUri(), CREATION_DAY, vf.createIRI(NPA_DATE_PREFIX + getDayString(timestamp)), ADMIN_GRAPH));
+				statements.add(vf.createStatement(np.getUri(), CREATION_MONTH, vf.createIRI(NPA_DATE_PREFIX + getMonthString(timestamp)), ADMIN_GRAPH));
+				statements.add(vf.createStatement(np.getUri(), CREATION_YEAR, vf.createIRI(NPA_DATE_PREFIX + getYearString(timestamp)), ADMIN_GRAPH));
+				statements.add(vf.createStatement(np.getUri(), DCTERMS.CREATED, vf.createLiteral(timestamp.getTime()), ADMIN_GRAPH));
 			} else {
-				st.add(vf.createStatement(np.getUri(), CREATION_DAY, vf.createIRI(NPA_DATE_PREFIX + "NONE"), ADMIN_GRAPH));
-				st.add(vf.createStatement(np.getUri(), CREATION_MONTH, vf.createIRI(NPA_DATE_PREFIX + "NONE"), ADMIN_GRAPH));
-				st.add(vf.createStatement(np.getUri(), CREATION_YEAR, vf.createIRI(NPA_DATE_PREFIX + "NONE"), ADMIN_GRAPH));
-				st.add(vf.createStatement(np.getUri(), DCTERMS.CREATED, vf.createLiteral(""), ADMIN_GRAPH));
+				statements.add(vf.createStatement(np.getUri(), CREATION_DAY, vf.createIRI(NPA_DATE_PREFIX + "NONE"), ADMIN_GRAPH));
+				statements.add(vf.createStatement(np.getUri(), CREATION_MONTH, vf.createIRI(NPA_DATE_PREFIX + "NONE"), ADMIN_GRAPH));
+				statements.add(vf.createStatement(np.getUri(), CREATION_YEAR, vf.createIRI(NPA_DATE_PREFIX + "NONE"), ADMIN_GRAPH));
+				statements.add(vf.createStatement(np.getUri(), DCTERMS.CREATED, vf.createLiteral(""), ADMIN_GRAPH));
 			}
 			if (containsNullCharacter) {
-				st.add(vf.createStatement(np.getUri(), NOTE, vf.createLiteral("contained NULL character"), ADMIN_GRAPH));
+				statements.add(vf.createStatement(np.getUri(), NOTE, vf.createLiteral("contained NULL character"), ADMIN_GRAPH));
 			}
-			conn.add(st);
+			conn.add(statements);
 		} catch (MalformedNanopubException ex) {
 			ex.printStackTrace();
 		}
@@ -94,16 +142,28 @@ public class NanopubModule {
 		return df.format(c.getTime());
 	}
 
+	private static IRI getBaseTrustyUri(Value v) {
+		if (!(v instanceof IRI)) return null;
+		String s = v.stringValue();
+		if (!s.matches(".*[^A-Za-z0-9\\-_]RA[A-Za-z0-9\\-_]{43}([^A-Za-z0-9\\\\-_][A-Za-z0-9\\-_\\.]{0,43})?")) {
+			return null;
+		}
+		return vf.createIRI(s.replaceFirst("^(.*[^A-Za-z0-9\\-_]RA[A-Za-z0-9\\-_]{43})([^A-Za-z0-9\\\\-_][A-Za-z0-9\\-_\\.]{0,43})?$", "$1"));
+	}
+
 	private static TimeZone timeZone = TimeZone.getTimeZone("UTC");
 	private static ValueFactory vf = SimpleValueFactory.getInstance();
 
 	public static final String NPA_DATE_PREFIX = "http://purl.org/nanopub/admin/date/";
 	public static final IRI ADMIN_GRAPH = vf.createIRI("http://purl.org/nanopub/admin/graph");
+	public static final IRI ADMIN_NETWORK_GRAPH = vf.createIRI("http://purl.org/nanopub/admin/networkGraph");
 	public static final IRI HAS_HEAD_GRAPH = vf.createIRI("http://purl.org/nanopub/admin/hasHeadGraph");
 	public static final IRI CREATION_DAY = vf.createIRI("http://purl.org/nanopub/admin/creationDay");
 	public static final IRI CREATION_MONTH = vf.createIRI("http://purl.org/nanopub/admin/creationMonth");
 	public static final IRI CREATION_YEAR = vf.createIRI("http://purl.org/nanopub/admin/creationYear");
 	public static final IRI NOTE = vf.createIRI("http://purl.org/nanopub/admin/note");
+	public static final IRI HAS_SUB_IRI = vf.createIRI("http://purl.org/nanopub/admin/hasSubIri");
+	public static final IRI REFERS_TO_NANOPUB = vf.createIRI("http://purl.org/nanopub/admin/refersToNanopub");
 	public static final IRI HAS_VALID_SIGNATURE_FOR_PUBLIC_KEY = vf.createIRI("http://purl.org/nanopub/admin/hasValidSignatureForPublicKey");
 
 }
